76379423689383a9062123e69b809359b0cd39b3
Edgar Abraham Ibarra <edgarpywy@gmail.com>
2026-09-03 02:49:05 -0500

    Configurar acceso estable con Tailscale

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  allowedDevOrigins: [
   "192.168.100.150",
-  "marina-lock-geographical-kind.trycloudflare.com",
+"dell15.tailc6838c.ts.net",
 ],
 };
 
